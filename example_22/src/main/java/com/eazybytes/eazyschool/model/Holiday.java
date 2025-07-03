0077f98e6cdd9a6484fec44f5693f80a3c684975
quang290401 <124598871+quang290401@users.noreply.github.com>
2025-07-03 16:02:56 +0700

    sửa lại security

--- example_22/src/main/java/com/eazybytes/eazyschool/model/Holiday.java
+++ example_22/src/main/java/com/eazybytes/eazyschool/model/Holiday.java
@@ -4,11 +4,9 @@ import lombok.Data;
 
 @Data
 public class Holiday {
-
     private final String day;
     private final String reason;
     private final Type type;
-
     public enum Type {
         FESTIVAL, FEDERAL
     }
