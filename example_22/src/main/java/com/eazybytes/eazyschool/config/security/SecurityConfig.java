dcee4f7e94924c8a063f5b2ec1cd5bcd85a338c8
quang290401 <124598871+quang290401@users.noreply.github.com>
2025-07-09 11:19:36 +0700

    sửa lại security

--- example_22/src/main/java/com/eazybytes/eazyschool/config/security/SecurityConfig.java
+++ example_22/src/main/java/com/eazybytes/eazyschool/config/security/SecurityConfig.java
@@ -38,7 +38,6 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
 
