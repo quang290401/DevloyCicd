82b27f6d7ae8d53ada30f01148e138104d006764
quang290401 <124598871+quang290401@users.noreply.github.com>
2025-07-09 11:21:29 +0700

    sửa lại securitys

--- example_22/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ example_22/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -23,6 +23,10 @@ public class ContactService {
         log.info(contact.toString());
         return isSaved;
     }
+    //cộng hai số nguyên
+    public int add(int a, int b) {
+        return a + b;
+    }
 
 
 
